b8f598c8db8e61d875a8d2ee94e2e42c9c51e1b5
dbruno2 <dbruno2@studenti.uninsubria.it>
2026-04-24 11:39:55 +0200

    impostazione struttura di base

---
+++ app/src/main/java/com/example/vaccinibiologicibrunosvezia/model/PatientInput.kt
@@ -0,0 +1,8 @@
+package com.example.vaccinibiologicibrunosvezia.model
+
+data class PatientInput(
+    val terapiaBiologica: String,
+    val eta: Int,
+    val condizioni: List<String>,
+    val storiaVaccinale: List<String>
+)
---
+++ app/src/main/java/com/example/vaccinibiologicibrunosvezia/viewmodel/VaccineViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.vaccinibiologicibrunosvezia.viewmodel
+
+class VaccineViewModel {
+
+}
