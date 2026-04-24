d01c3c4366eee4b098c197b7527104862104b100
dbruno2 <dbruno2@studenti.uninsubria.it>
2026-04-24 10:15:56 +0200

    prova FINALE per sebastiano

--- app/src/main/java/com/example/vaccinibiologicibrunosvezia/MainActivity.kt
+++ app/src/main/java/com/example/vaccinibiologicibrunosvezia/MainActivity.kt
@@ -46,4 +46,4 @@ fun GreetingPreview() {
     }
 }
 
-//prova non funziona niente Push da Sebastiano Svezia aka PROVAFINALE
+//prova non funziona niente Push da Sebastiano Svezia aka PROVAFINALE aka VERAPROVAFINALE
