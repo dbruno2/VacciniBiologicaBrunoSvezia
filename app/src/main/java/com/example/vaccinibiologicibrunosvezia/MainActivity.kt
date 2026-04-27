a99c53e67a0584a2585d7c9cd0e615eaa7d7a69f
ssvezia <ssvezia@studenti.uninsubria.it>
2026-04-27 12:41:35 +0200

    fix bottoni cambio lingua pt2

--- app/src/main/java/com/example/vaccinibiologicibrunosvezia/MainActivity.kt
+++ app/src/main/java/com/example/vaccinibiologicibrunosvezia/MainActivity.kt
@@ -1,70 +1,54 @@
 package com.example.vaccinibiologicibrunosvezia
-//ciao
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import android.media.Image
+
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.os.LocaleListCompat
 import com.example.vaccinibiologicibrunosvezia.model.PatientInput
 import com.example.vaccinibiologicibrunosvezia.model.Vaccine
 import com.example.vaccinibiologicibrunosvezia.ui.theme.VacciniBiologiciBrunoSveziaTheme
-import com.example.vaccinibiologicibrunosvezia.ui.theme.Typography
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             VacciniBiologiciBrunoSveziaTheme {
                 SchermataPrincipale()
-/*
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    Text(text = "Terapia biologica")
-                    Text(text = "Età")
-                    Text(text = "Condizioni cliniche")
-                }
-*/
-                }
             }
         }
     }
-
-
-
+}
+//prova
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SchermataPrincipale() {
-
     var expanded by remember { mutableStateOf(false) }
 
-    val terapie = listOf(
-        "anti-TNF",
-        "anti-IL17",
-        "anti-IL23",
-        "altri immunosoppressori"
-    )
-
-    var terapiaSelezionata by remember { mutableStateOf("") }
-    var etaText by remember { mutableStateOf("") }
-    var condizioniText by remember { mutableStateOf("") }
-   // var vacciniText by remember { mutableStateOf("") }
-
-    var risultato by remember { mutableStateOf("") }
+    val terapie = stringArrayResource(R.array.terapie)
+    val context = LocalContext.current
+    val activity = context as? Activity
+    
+    var terapiaSelezionata by rememberSaveable { mutableStateOf("") }
+    var etaText by rememberSaveable { mutableStateOf("") }
+    var condizioniText by rememberSaveable { mutableStateOf("") }
+    var risultato by rememberSaveable { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -74,21 +58,19 @@ fun SchermataPrincipale() {
             .padding(bottom = 80.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-
     ) {
-        Text(text = "VACCINI", fontSize = 40.sp)
+        Text(stringResource(R.string.title), fontSize = 40.sp)
         Spacer(modifier = Modifier.height(20.dp))
 
         ExposedDropdownMenuBox(
             expanded = expanded,
             onExpandedChange = { expanded = !expanded }
         ) {
-
             OutlinedTextField(
                 value = terapiaSelezionata,
                 onValueChange = {},
                 readOnly = true,
-                label = { Text("Terapia biologica") },
+                label = { Text(stringResource(R.string.condizione)) },
                 trailingIcon = {
                     ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
                 },
@@ -116,7 +98,7 @@ fun SchermataPrincipale() {
         OutlinedTextField(
             value = etaText,
             onValueChange = { etaText = it },
-            label = { Text("Età") }
+            label = { Text(stringResource(R.string.age)) }
         )
 
         Spacer(modifier = Modifier.height(10.dp))
@@ -124,31 +106,28 @@ fun SchermataPrincipale() {
         OutlinedTextField(
             value = condizioniText ,
             onValueChange = { condizioniText  = it },
-            label = { Text("Condizione clinica") }
+            label = { Text(stringResource(R.string.condizione)) }
         )
 
         Spacer(modifier = Modifier.height(10.dp))
 
         Button(onClick = {
-
             val paziente = PatientInput(
                 terapiaBiologica = terapiaSelezionata,
                 eta = etaText.toIntOrNull() ?: 0,
                 condizioni = condizioniText.split(",").map { it.trim() },
-                storiaVaccinale = condizioniText.split(",").map { Vaccine(it.trim()) } //da modificare appena capiamo cosa fare con storia vaccinale
+                storiaVaccinale = condizioniText.split(",").map { Vaccine(it.trim()) } 
             )
-
-
-
+            // TODO: Implement logic for risultato
         }) {
-            Text("Calcola")
+            Text(stringResource(R.string.calcola))
         }
 
         Spacer(modifier = Modifier.height(20.dp))
 
         Text(text = risultato)
 
-        Spacer( modifier = Modifier.height(110.dp))
+        Spacer(modifier = Modifier.height(110.dp))
 
         Box(
             modifier = Modifier
@@ -159,25 +138,25 @@ fun SchermataPrincipale() {
                 modifier = Modifier.align(Alignment.BottomCenter),
                 horizontalArrangement = Arrangement.spacedBy(15.dp),
                 verticalAlignment = Alignment.Bottom
-
             ) {
-                Button(onClick = { }) {
-                    Text("ITA")
+                Button(onClick = {
+                    changeLanguage(activity, "it")
+                }) {
+                    Text(stringResource(R.string.ita))
                 }
 
-                Button(onClick = { }) {
-                    Text("ENG")
+                Button(onClick = {
+                    changeLanguage(activity, "en")
+                }) {
+                    Text(stringResource(R.string.eng))
                 }
             }
         }
     }
-
-
 }
 
-@Composable
-fun SchermataCalcola(){
-
+fun changeLanguage(activity: Activity?, language: String) {
+    val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(language)
+    AppCompatDelegate.setApplicationLocales(appLocale)
+    activity?.recreate()
 }
-
-//prova non funziona niente Push da Sebastiano Svezia aka PROVAFINALE aka VERAPROVAFINALE
